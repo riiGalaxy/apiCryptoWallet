2606c7dca8d87bdf9e17670ba786b58cf2643506
riiGalaxy <riimolpeceres@gmail.com>
2022-03-11 10:44:05 +0100

    done

--- src/portfolio/controller.js
+++ src/portfolio/controller.js
@@ -110,6 +110,7 @@ const controller = (() => {
   //delete investment
   router.delete("/investment/:id", async (req, res) => {
     const { id } = req.params;
+    console.log("api borrandoooooo");
 
     try {
       const investment = await PortfolioModel.deleteOne({ _id: id });
